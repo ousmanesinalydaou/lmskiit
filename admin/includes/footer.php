6079b68c628bb35456eb4754de84436c47aae146
OUSMANE DAOU <ousmanedaou@live.fr>
2023-04-28 11:20:54 +0530

    Adding the project's files

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2023 Online Library Management System |<a href="https://www.daouenterprise.com" target="_blank"  > Designed by : Group 3 of BSc</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
